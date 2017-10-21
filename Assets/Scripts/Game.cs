e500e90b4294c2ba076a1940700a68f6db5b2c25
alaurentinoofficial <alaurentino.br@gmail.com>
2017-10-21 18:20:45 -0200

    Update

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+
+	public static GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		Game.player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Point.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class Point : MonoBehaviour
+{
+	public Material Normal;
+	public Material Hover;
+
+
+	MeshRenderer mr;
+
+	void Start ()
+	{
+		mr = GetComponent<MeshRenderer> ();
+	}
+
+	public void Click()
+	{
+		Game.player.transform.position = new Vector3(Game.player.transform.position.x, Game.player.transform.position.y, transform.position.z);
+	}
+
+	public void MouseHover(bool enter)
+	{
+		if (enter)
+		{
+			mr.material = Hover;
+			transform.localScale = new Vector3(.7f, .7f, .7f);
+		}
+		else
+		{
+			mr.material = Normal;
+			transform.localScale = new Vector3(.5f, .5f, .5f);
+		}
+	}
+}
